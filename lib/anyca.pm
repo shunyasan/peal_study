26625181de1cc38e9b544ea812a6348de97fd798
高橋駿也 <>
2022-06-10 13:47:03 +0900

    initial import

---
+++ lib/anyca.pm
@@ -0,0 +1,47 @@
+package anyca;
+use strict;
+use warnings;
+use utf8;
+our $VERSION='0.01';
+use 5.008001;
+use anyca::DB::Schema;
+use anyca::DB;
+
+use parent qw/Amon2/;
+# Enable project local mode.
+__PACKAGE__->make_local_context();
+
+my $schema = anyca::DB::Schema->instance;
+
+sub db {
+    my $c = shift;
+    if (!exists $c->{db}) {
+        my $conf = $c->config->{DBI}
+            or die "Missing configuration about DBI";
+        $c->{db} = anyca::DB->new(
+            schema       => $schema,
+            connect_info => [@$conf],
+            # I suggest to enable following lines if you are using mysql.
+            # on_connect_do => [
+            #     'SET SESSION sql_mode=STRICT_TRANS_TABLES;',
+            # ],
+        );
+    }
+    $c->{db};
+}
+
+1;
+__END__
+
+=head1 NAME
+
+anyca - anyca
+
+=head1 DESCRIPTION
+
+This is a main context class for anyca
+
+=head1 AUTHOR
+
+anyca authors.
+
---
+++ t/00_compile.t
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+use anyca;
+use anyca::Web;
+use anyca::Web::View;
+use anyca::Web::ViewFunctions;
+
+use anyca::DB::Schema;
+use anyca::Web::Dispatcher;
+
+
+pass "All modules can load.";
+
+done_testing;
